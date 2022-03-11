2fd4973d800a85a8cf39905066241b0631f845fa
ThatGuyTsholo <pmolefe25@gmail.com>
2022-03-10 16:41:34 -0800

    Cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
-#include <stdio.h> 
-
-/** 
- * main - entry point 
- * Description: prints "\"Programming is like a multilingual puzzle"
+#include <stdio.h>
+/**
+ * main - main block
  * Return: 0
- * */
-
-int main (void)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
